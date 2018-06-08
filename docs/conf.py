6dff87e4dd203c7e098c4954135628b1c8f4059a
Aaron Meurer <asmeurer@gmail.com>
2018-06-08 14:29:27 -0400

    Add copybutton.js
    This makes the code blocks have a >>> button on the top right that hides
    the >>> and outputs, so the code can be copied.

--- docs/conf.py
+++ docs/conf.py
@@ -78,7 +78,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -131,7 +130,7 @@ html_sidebars = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
