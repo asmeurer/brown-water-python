63039c283a9841b4b0416be81d9056e0e96cb95c
Aaron Meurer <asmeurer@gmail.com>
2020-07-15 12:37:48 -0600

    Make warnings give errors in the Sphinx build

--- docs/conf.py
+++ docs/conf.py
@@ -68,6 +68,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 
+# Make Sphinx warn about bad cross references
+nitpicky = True
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
